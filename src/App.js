37a4a4021f0096cf98dbb5c88e40890e3fba883e
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2021-04-15 22:53:26 -0600

    Autocomplete component with tests

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import AutocompleteInput from './AutocompleteInput';
+import AutocompleteList from './AutocompleteList';
 
-function App() {
+export const names = ['Adam', 'Anthony', 'Bob', 'Billy', 'Carol', 'Cathy', 'Kevin', 'Kyle', 'Frank', 'Fred'];
+
+const App = () => {
+  const [value, setValue] = useState('');
+  const [list, setList] = useState('');
+  
+  const suggestions = [];
+
+  const setInputValue = (suggestion) => {
+    setValue(suggestion);
+  }
+
+  const autocomplete = (event) => {
+    setValue(event.target.value);
+    names.forEach(name => {
+      if (name.substr(0, event.target.value.length).toUpperCase() === event.target.value.toUpperCase()) {
+        suggestions.push(name);
+        setList(
+          suggestions.map((suggestion, index) => {
+            return <p key={index} onClick={() => setInputValue(suggestion)}>{suggestion}</p>
+          })
+        );
+        if (event.target.value.length === 0) {
+          setList([]);
+        }
+      }
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="containerCenter">
+      <AutocompleteInput autocomplete={autocomplete} value={value} />
+      <AutocompleteList list={list} />
     </div>
   );
 }
